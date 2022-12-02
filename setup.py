0aa55b0ea8c2dba5032fa4f0c19f2e97c665cfd6
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2022-12-03 00:50:40 +0100

    created new directory snowflake, added requirements, setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="snowflake",
+    version="0.1",
+    author="pallavi-ghimire",
+    author_email="pallavi.ghimire.39@gmail.com",
+    packages=find_packages(),
+    install_requires=["numpy", "turtles"]
+)
